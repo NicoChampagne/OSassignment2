d67735c176adc8f2b5471a5c67d1be675bb4d88c
nicoChampagne <nicolas.b.champagne@gmail.com>
2018-03-05 14:55:41 -0500

    Finished task4 and task5, refactored tasak3 and reorganized

---
+++ src/a2_27043651/task1/BlockStack1.java
@@ -0,0 +1,139 @@
+package a2_27043651.task1;
+
+/**
+ * Class BlockStack
+ * Implements character block stack and operations upon it.
+ *
+ * @author Nicolas Brodeur-Champagne
+ * With base assignment from Serguei A. Mokhov, mokhov@cs.concordia.ca;
+ */
+class BlockStack1
+{
+	//Added for Task1
+	public static int accessCounter =0;
+	
+	/**
+	 * # of letters in the English alphabet + 2
+	 */
+	public static final int MAX_SIZE = 28;
+
+	/**
+	 * Default stack size
+	 */
+	public static final int DEFAULT_SIZE = 6;
+
+	/**
+	 * Current size of the stack
+	 */
+	public int iSize = DEFAULT_SIZE;
+
+	/**
+	 * Current top of the stack
+	 */
+	public int iTop  = 3;
+
+	/**
+	 * stack[0:5] with four defined values
+	 */
+	public char acStack[] = new char[] {'a', 'b', 'c', 'd', '$', '$'};
+
+	/**
+	 * Default constructor
+	 */
+	public BlockStack1()
+	{
+	}
+
+	/**
+	 * Supplied size
+	 */
+	public BlockStack1(final int piSize)
+	{
+
+
+                if(piSize != DEFAULT_SIZE)
+		{
+			this.acStack = new char[piSize];
+
+			// Fill in with letters of the alphabet and keep
+			// 2 free blocks
+			for(int i = 0; i < piSize - 2; i++)
+				this.acStack[i] = (char)('a' + i);
+
+			this.acStack[piSize - 2] = this.acStack[piSize - 1] = '$';
+
+			this.iTop = piSize - 3;
+                        this.iSize = piSize;
+		}
+	}
+
+	/**
+	 * Picks a value from the top without modifying the stack
+	 * @return top element of the stack, char
+	 */
+	public char pick()
+	{
+		accessCounter++;
+		return this.acStack[this.iTop];
+	}
+
+	/**
+	 * Returns arbitrary value from the stack array
+	 * @return the element, char
+	 */
+	public char getAt(final int piPosition)
+	{
+		accessCounter++;
+		return this.acStack[piPosition];
+	}
+
+	/**
+	 * Standard push operation
+	 */
+	public void push(final char pcBlock)
+	{
+		accessCounter++;
+		this.acStack[++this.iTop] = pcBlock;
+	}
+
+	/**
+	 * Standard pop operation
+	 * @return ex-top element of the stack, char
+	 */
+	public char pop()
+	{
+		accessCounter++;
+		char cBlock = this.acStack[this.iTop];
+		this.acStack[this.iTop--] = '$'; // Leave prev. value undefined
+		return cBlock;
+	}
+	
+	//Added for Task1
+	public int getITop()
+	{
+		accessCounter++;
+		return iTop;
+	}
+	
+	//Added for Task1
+	public int getISize()
+	{
+		accessCounter++;
+		return iSize;
+	}
+	
+	//Added for Task1
+	public int getAccessCounter()
+	{
+		return accessCounter;
+	}
+	
+	//Added for Task1
+	public boolean isEmpty()
+	{
+		accessCounter++;
+		return (this.iTop == -1);
+	}
+}
+
+// EOF
---
+++ src/a2_27043651/task5/BlockManager5.java
@@ -0,0 +1,390 @@
+package a2_27043651.task5;
+	import a2_27043651.task5.BlockStack5;
+// Import (aka include) some stuff.
+	import common.*;
+
+	/**
+	 * Class BlockManager
+	 * Implements character block "manager" and does twists with threads.
+	 *
+	 * @author Nicolas Brodeur-Champagne
+	 * With base assignment from Serguei A. Mokhov, mokhov@cs.concordia.ca;
+	 */
+
+	 
+	public class BlockManager5
+	{
+		/**
+		 * The stack itself
+		 */
+		private static BlockStack5 soStack = new BlockStack5();
+
+		/**
+		 * Number of threads dumping stack
+		 */
+		private static final int NUM_PROBERS = 4;
+
+		/**
+		 * Number of steps they take
+		 */
+		private static int siThreadSteps = 5;
+
+		/**
+		 * For atomicity
+		 */
+		private static Semaphore mutex = new Semaphore(1);
+
+		 
+		//Changed/added semaphore S1 for Task4's barrier Synchronization.
+		/**
+		 * s1 is to make sure phase I for all is done before any phase II begins
+		 */
+		private static Semaphore s1 = new Semaphore(0);
+
+		/**
+		 * s2 is for use in conjunction with Thread.turnTestAndSet() for phase II proceed
+		 * in the thread creation order
+		 */
+		private static Semaphore s2 = new Semaphore(1);
+		
+		
+		//added 4 integers to establish counts for barrier Synchronization.
+		private static final int N = 10;
+		private static int ab = 0;
+
+
+		// The main()
+		public static void main(String[] argv)
+		{
+			try
+			{
+				// Some initial stats...
+				System.out.println("Main thread starts executing.");
+				System.out.println("Initial value of top = " + soStack.getITop() + ".");
+				System.out.println("Initial value of stack top = " + soStack.pick() + ".");
+				System.out.println("Main thread will now fork several threads.");
+
+				/*
+				 * The birth of threads
+				 */
+				AcquireBlock ab1 = new AcquireBlock();
+				AcquireBlock ab2 = new AcquireBlock();
+				AcquireBlock ab3 = new AcquireBlock();
+
+				System.out.println("main(): Three AcquireBlock threads have been created.");
+
+				ReleaseBlock rb1 = new ReleaseBlock();
+				ReleaseBlock rb2 = new ReleaseBlock();
+				ReleaseBlock rb3 = new ReleaseBlock();
+
+				System.out.println("main(): Three ReleaseBlock threads have been created.");
+
+				// Create an array object first
+				CharStackProber	aStackProbers[] = new CharStackProber[NUM_PROBERS];
+
+				// Then the CharStackProber objects
+				for(int i = 0; i < NUM_PROBERS; i++)
+					aStackProbers[i] = new CharStackProber();
+
+				System.out.println("main(): CharStackProber threads have been created: " + NUM_PROBERS);
+
+				/*
+				 * Twist 'em all
+				 */
+				ab1.start();
+				aStackProbers[0].start();
+				rb1.start();
+				aStackProbers[1].start();
+				ab2.start();
+				aStackProbers[2].start();
+				rb2.start();
+				ab3.start();
+				aStackProbers[3].start();
+				rb3.start();
+
+				System.out.println("main(): All the threads are ready.");
+
+				/*
+				 * Wait by here for all forked threads to die
+				 */
+				ab1.join();
+				ab2.join();
+				ab3.join();
+
+				rb1.join();
+				rb2.join();
+				rb3.join();
+
+				for(int i = 0; i < NUM_PROBERS; i++)
+					aStackProbers[i].join();
+
+				// Some final stats after all the child threads terminated...
+				System.out.println("System terminates normally.");
+				System.out.println("Final value of top = " + soStack.getITop() + ".");
+				System.out.println("Final value of stack top = " + soStack.pick() + ".");
+				System.out.println("Final value of stack top-1 = " + soStack.getAt(soStack.getITop() - 1) + ".");
+				System.out.println("Stack access count = " + soStack.getAccessCounter());
+
+				System.exit(0);
+			}
+			catch(InterruptedException e)
+			{
+				System.err.println("Caught InterruptedException (internal error): " + e.getMessage());
+				e.printStackTrace(System.err);
+			}
+			catch(Exception e)
+			{
+				reportException(e);
+			}
+			finally
+			{
+				System.exit(1);
+			}
+		} // main()
+
+
+		/**
+		 * Inner AcquireBlock thread class.
+		 */
+		static class AcquireBlock extends BaseThread
+		{
+			/**
+			 * A copy of a block returned by pop().
+	                 * @see BlocStack#pop()
+			 */
+			private char cCopy;
+
+			//TASK4 added barrier synchronization.
+			public void run()
+			{
+			
+				mutex.P();
+				ab++;
+				
+				System.out.println("AcquireBlock thread [TID=" + this.iTID + "] starts executing.");
+
+
+				phase1();
+
+
+				try
+				{
+					System.out.println("AcquireBlock thread [TID=" + this.iTID + "] requests Ms block.");
+
+					this.cCopy = soStack.pop();
+					
+					System.out.println
+					(
+						"AcquireBlock thread [TID=" + this.iTID + "] has obtained Ms block " + this.cCopy +
+						" from position " + (soStack.getITop() + 1) + "."
+					);
+
+
+					System.out.println
+					(
+						"Acq[TID=" + this.iTID + "]: Current value of top = " +
+						soStack.getITop() + "."
+					);
+
+					System.out.println
+					(
+						"Acq[TID=" + this.iTID + "]: Current value of stack top = " +
+						soStack.pick() + "."
+					);
+				}
+				catch(Exception e)
+				{
+					reportException(e);
+					System.exit(1);
+				}
+				finally
+				{
+					if(ab==N)
+						s1.V();
+					mutex.V();
+				}
+				
+				
+				s1.P();
+				s1.V();
+				s2.P(); // From this point, only one thread can enter phase 2 at a time. 
+				while (!turnTestAndSet())
+				{
+					s2.V(); // These two lines are to give other
+					s2.P(); // threads a chance to check their turn.
+				}
+				phase2();
+				s2.V();
+				
+
+				System.out.println("AcquireBlock thread [TID=" + this.iTID + "] terminates.");
+			
+			}
+		} // class AcquireBlock
+
+
+		/**
+		 * Inner class ReleaseBlock.
+		 */
+		static class ReleaseBlock extends BaseThread
+		{
+			
+			/**
+			 * Block to be returned. Default is 'a' if the stack is empty.
+			 */
+			private char cBlock = 'a';
+
+			//TASK4 added barrier synchronization
+			public void run()
+			{
+				
+				mutex.P();
+				ab++;
+				
+				
+				System.out.println("ReleaseBlock thread [TID=" + this.iTID + "] starts executing.");
+
+
+				phase1();
+
+
+				try
+				{
+					if(soStack.isEmpty() == false)
+						this.cBlock = (char)(soStack.pick() + 1);
+
+
+					System.out.println
+					(
+						"ReleaseBlock thread [TID=" + this.iTID + "] returns Ms block " + this.cBlock +
+						" to position " + (soStack.getITop() + 1) + "."
+					);
+
+			
+					soStack.push(this.cBlock);
+					
+					System.out.println
+					(
+						"Rel[TID=" + this.iTID + "]: Current value of top = " +
+						soStack.getITop() + "."
+					);
+
+					System.out.println
+					(
+						"Rel[TID=" + this.iTID + "]: Current value of stack top = " +
+						soStack.pick() + "."
+					);
+				}
+				catch(Exception e)
+				{
+					reportException(e);
+					System.exit(1);
+				}
+				finally
+				{
+					if(ab==N)
+						s1.V();
+					mutex.V();
+				}
+				
+				s1.P();
+				s1.V();
+				
+				s2.P(); // From this point, only one thread can enter phase 2 at a time. 
+				while (!turnTestAndSet())
+				{
+					s2.V(); // These two lines are to give other
+					s2.P(); // threads a chance to check their turn.
+				}
+				phase2();
+				s2.V();
+			
+
+				System.out.println("ReleaseBlock thread [TID=" + this.iTID + "] terminates.");
+				
+			}
+		} // class ReleaseBlock
+
+
+		//TASK4 added barrier synchronization and a mutex for stack prober to get clean output
+		/**
+		 * Inner class CharStackProber to dump stack contents.
+		 */
+		static class CharStackProber extends BaseThread
+		{
+		
+			public void run()
+			{
+				
+				mutex.P();
+				ab++;
+					
+				phase1();
+
+
+				try
+				{
+					
+					for(int i = 0; i < siThreadSteps; i++)
+					{
+						System.out.print("Stack Prober [TID=" + this.iTID + "]: Stack state: ");
+
+						// [s] - means ordinay slot of a stack
+						// (s) - current top of the stack
+						for(int s = 0; s < soStack.getISize(); s++)
+							System.out.print
+							(
+								(s == BlockManager5.soStack.getITop() ? "(" : "[") +
+								BlockManager5.soStack.getAt(s) +
+								(s == BlockManager5.soStack.getITop() ? ")" : "]")
+							);
+
+						System.out.println(".");
+
+					}
+					
+				}
+				catch(Exception e)
+				{
+					reportException(e);
+					System.exit(1);
+				}
+				finally
+				{
+					if(ab==N)
+						s1.V();
+					mutex.V();
+				}
+
+				s1.P();
+				s1.V();
+				
+				s2.P(); // From this point, only one thread can enter phase 2 at a time. 
+				while (!turnTestAndSet())
+				{
+					s2.V(); // These two lines are to give other
+					s2.P(); // threads a chance to check their turn.
+				}
+				phase2();
+				s2.V();
+				
+
+				
+			}
+		} // class CharStackProber
+
+
+		/**
+		 * Outputs exception information to STDERR
+		 * @param poException Exception object to dump to STDERR
+		 */
+		private static void reportException(Exception poException)
+		{
+			System.err.println("Caught exception : " + poException.getClass().getName());
+			System.err.println("Message          : " + poException.getMessage());
+			System.err.println("Stack Trace      : ");
+			poException.printStackTrace(System.err);
+		}
+	} // class BlockManager
+
+	// EOF
